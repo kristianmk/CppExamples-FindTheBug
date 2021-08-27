0d027ada0726d8f679b60688dead4d006d746971
Kristian Muri Knausgård <1713062+kristianmk@users.noreply.github.com>
2021-08-27 08:00:16 +0200

    Removed obsolete file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int integerDivision(int num, int den);
+
+
+
+int main() {
+
+    const int a(50);
+    const int b(25);
+
+    std::cout << "Starting up.." << std::endl;
+
+    for (int i = 0; i<=25; ++i)
+    {
+        const int result =  integerDivision(a, b-i);
+        std::cout << "  result = " << result <<  std::endl;
+    }
+
+    std::cout << "Completed.." << std::endl;
+
     return 0;
+}
+
+
+int integerDivision(int a, int b)
+{
+    return a / b;
 }
