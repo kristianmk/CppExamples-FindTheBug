72437eae0ba41bf987046f93c4f299f616ca56a7
kristianmk <1713062+kristianmk@users.noreply.github.com>
2021-08-27 08:17:35 +0200

    Update main.cpp
    Removed some blank lines.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 
-
 int integerDivision(int num, int den);
 
-
-
 int main() {
-
     const int a(50);
     const int b(25);
 
     std::cout << "Starting up.." << std::endl;
-
     for (int i = 0; i<=25; ++i)
     {
         const int result =  integerDivision(a, b-i);
         std::cout << "  result = " << result <<  std::endl;
     }
-
     std::cout << "Completed.." << std::endl;
-
     return 0;
 }
-
 
 int integerDivision(int a, int b)
 {
