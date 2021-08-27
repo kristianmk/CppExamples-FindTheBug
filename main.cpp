059b87987388d4fbc894c3c051c6435b5d5c72af
kristianmk <1713062+kristianmk@users.noreply.github.com>
2021-08-27 15:56:24 +0200

    Update main.cpp
    Removed more newlines.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@ int integerDivision(int num, int den);
 
 int main() {
     const int a(50); const int b(25);
-
     std::cout << "Starting up.." << std::endl;
-    for (int i = 0; i<=25; ++i)
-    {
+    for (int i = 0; i<=25; ++i) {
         const int result =  integerDivision(a, b-i);
         std::cout << "  result = " << result <<  std::endl;
     }
